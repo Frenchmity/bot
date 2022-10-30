9851e5b496b86f94e61820cc20c70556e1062c73
mugman <53713683+nature174@users.noreply.github.com>
2022-10-29 17:20:29 -0700

    add new cog for commands

--- bot.py
+++ bot.py
@@ -1,11 +1,22 @@
+"""Discord bot for Unmity"""
+
 import discord
 from discord.ext import commands
+
+from cogs import cmds
+
 import config
+
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
 bot = commands.Bot(intents=intents, command_prefix="!")
+
+@bot.event
+async def on_ready():
+    """When the bot is connected to Discord and ready to start doing stuff"""
+    await cmds.setup(bot)
 
 bot.run(config.token)
---
+++ cogs/cmds.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+import discord
+
+class Cmds(commands.Cog):
+    """The description for Commands goes here."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.hybrid_command()
+    async def hello(self, ctx):
+        await ctx.send("Hello, world!")
+
+async def setup(bot):
+    await bot.add_cog(Cmds(bot))
