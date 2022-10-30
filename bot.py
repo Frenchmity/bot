6edec33372ebd9d9d4d1392a168b56ec137c4984
mugman <53713683+nature174@users.noreply.github.com>
2022-10-30 16:01:18 -0700

    [bot] Add connected message

--- bot.py
+++ bot.py
@@ -14,11 +14,14 @@ intents.message_content = True
 
 bot = commands.Bot(intents=intents, command_prefix="!")
 
+
 @bot.event
 async def on_ready():
     """When the bot is connected to Discord and ready to start doing stuff"""
     await cmds.setup(bot)
     await logs.setup(bot)
+    print(f"Connected as {bot.user}")
+
 
 @bot.command()
 @commands.is_owner()
@@ -27,5 +30,6 @@ async def sync_commands(ctx):
     bot.tree.copy_global_to(guild=discord.Object(id=1036052928806518824))
     await bot.tree.sync(guild=discord.Object(id=1036052928806518824))
     await ctx.send("Commands synced!")
+
 
 bot.run(config.token)
