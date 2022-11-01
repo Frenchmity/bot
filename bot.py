a66bb146ec4784ee7235011cc4f600c14bfff6d0
mugman <53713683+nature174@users.noreply.github.com>
2022-11-01 12:11:42 -0700

    [bot] Testing mode

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 """Discord bot for Unmity"""
 
+# To get environment variables for checking if testing mode
+from os import environ
+
 import discord
 from discord.ext import commands
 
@@ -12,7 +15,15 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(intents=intents, command_prefix="!")
+# For testing, use a different prefix for commands
+testing_mode = environ.get("UNMITY_TESTING")
+if testing_mode:
+    command_prefix="?!"
+    print("[Bot] Testing Mode")
+else:
+    command_prefix="!"
+
+bot = commands.Bot(intents=intents, command_prefix=command_prefix)
 
 
 @bot.event
@@ -20,22 +31,16 @@ async def on_ready():
     """When the bot is connected to Discord and ready to start doing stuff"""
     await cmds.setup(bot)
     await logs.setup(bot)
-    print(f"Connected as {bot.user}")
+    print(f"[Bot] Connected as {bot.user}")
 
 
-@bot.command()
+@bot.command(hidden=True)
 @commands.is_owner()
 async def sync_commands(ctx):
     """Synchronize slash commands to the Unmity server"""
     bot.tree.copy_global_to(guild=discord.Object(id=1036052928806518824))
     await bot.tree.sync(guild=discord.Object(id=1036052928806518824))
     await ctx.send("Commands synced!")
-    print(f"Commands synced by {ctx.author}")
-
-@bot.command()
-@commands.is_owner()
-async def stop(ctx):
-    await ctx.send("Commencing shutdown")
-    await bot.close()
+    print(f"[Bot] Commands synced by {ctx.author}")
 
 bot.run(config.token)
